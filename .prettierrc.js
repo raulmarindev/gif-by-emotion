b1bc5bc825902759abfb846a106f5214ed884dc3
Raúl Marín <rmarin82@gmail.com>
2020-07-20 18:44:41 +0200

    Add eslint, prettier and font awesome.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   semi: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
+  endOfLine: 'auto',
 };
