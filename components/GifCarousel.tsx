7c337443ba5cbeac9c596e4aa661b68a39793c55
Raúl Marín <rmarin82@gmail.com>
2020-07-20 22:54:08 +0200

    Add placeholder for our carousel component.

---
+++ components/GifCarousel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GifCarousel = () => {
+  return (
+    <div className="flex justify-between">
+      <button className="text-3xl">Prev</button>
+      <img alt="" className="" src="" />
+      <button className="text-3xl">Next</button>
+    </div>
+  );
+};
+
+export default GifCarousel;
