124bbbaabaa8ea6bbc467b86dbc2369533f784e8
Raúl Marín <rmarin82@gmail.com>
2020-07-26 23:22:28 +0200

    Fix  _document.tsx problems.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,21 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
 
-  render() {
+  render(): JSX.Element {
     return (
       <Html lang="en">
         <Head />
