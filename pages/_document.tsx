5865a6e0319b58c7dc1784a5073c8a8766f461f4
Raúl Marín <rmarin82@gmail.com>
2020-07-26 23:18:59 +0200

    Fix code issue with prettier.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,5 @@
-import Document, {
-    Head,
-    Html,
-    Main,
-    NextScript
-    } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
